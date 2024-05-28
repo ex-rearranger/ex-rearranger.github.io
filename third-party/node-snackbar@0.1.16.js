fb80ff28f17cb122beccb4de89a4c935d5662fcf
extreme-rearranger <pamela7384@snu.ac.kr>
2024-05-28 22:42:34 +0900

    Site updated: 2024-05-28 22:42:34

--- third-party/node-snackbar@0.1.16.js
+++ third-party/node-snackbar@0.1.16.js
@@ -69,10 +69,10 @@
       var $p = document.createElement('p');
       $p.style.margin = 0;
       $p.style.padding = 0;
-      $p.style.color = options.textColor;
-      $p.style.fontSize = '14px';
-      $p.style.fontWeight = 300;
-      $p.style.lineHeight = '1em';
+    //   $p.style.color = options.textColor;
+    //   $p.style.fontSize = '14px';
+    //   $p.style.fontWeight = 300;
+    //   $p.style.lineHeight = '1em';
       $p.innerHTML = options.text;
       Snackbar.snackbar.appendChild($p);
       Snackbar.snackbar.style.background = options.backgroundColor;
